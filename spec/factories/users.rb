252f4da77598978ee5df6ba8e20785ad49261740
Tsunemichi Harada <fox10225fox@gmail.com>
2018-09-15 02:22:14 +0900

    p55

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     first_name "Aaron"
     last_name "Summer"
-    email "tester4@example.com"
+    sequence(:email) {|n| "tester#{n}@example.com"}
     password "dottle-nouveau-pavilon-tights-furze"
   end
 end
