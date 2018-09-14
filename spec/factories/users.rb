b9f8937a8ef929b7611247daff5427f7337a69aa
Tsunemichi Harada <fox10225fox@gmail.com>
2018-09-15 02:20:29 +0900

    p54

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    first_name "Aaron"
+    last_name "Summer"
+    email "tester4@example.com"
+    password "dottle-nouveau-pavilon-tights-furze"
+  end
+end
